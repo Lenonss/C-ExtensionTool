93bb8250adf6085c5f962486c9e621d4634b0367
Liuzilong <goi82a@163.com>
2021-09-16 12:27:26 +0800

    新增输出xml文件功能，使dll被引用后可以显示对应注释

--- CSharpExtensionTool/CSharpExtensionTool/ListEx.cs
+++ CSharpExtensionTool/CSharpExtensionTool/ListEx.cs
@@ -6,19 +6,23 @@ namespace CSharpExtensionTool.ListExtension
     public static class ListEx
     {
         /// <summary>
-        /// 指定值初始化List数组,可指定数组容量
+        /// 指定值初始化List数组,可指定数组容量。
+        /// 指定类型不可为引用类型
         /// </summary>
-        /// <typeparam name="T">数组类型</typeparam>
+        /// <typeparam name="T">数组类型，限定为struct</typeparam>
         /// <param name="selfList">数组</param>
         /// <param name="Value">指定值</param>
         /// <param name="capcity">List.Count=0才起效</param>
         /// <returns></returns>
-        public static bool InitListWithValue<T>(this List<T> selfList, T Value, int capcity = 0)
+        public static bool InitListWithValue<T>(this List<T> selfList, T Value, int capcity = 0) where T : struct
         {
+            //数组不为空
             if (selfList == null)
             {
                 return false;
             }
+            //T的格式正确
+            
             //记录
             capcity = selfList.Count > 0 ? selfList.Count : capcity;
             //清空
